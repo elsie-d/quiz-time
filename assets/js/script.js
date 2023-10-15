ea20e86d2e7bcbbda6dccfef6ce324d125310493
Elsie <elsie.dh@gmail.com>
2023-10-14 18:32:23 -0700

    reset CSS and JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,14 @@
-/*
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-*/
-
-
-const startBtn = document.getElementById('start')
-const answers = document.getElementById('answerBtn')
-const 
-startBtn.addEventListener('click', startQuiz)
+// QUIZ QUESTIONS
 
 
 var questions = [
     {
-        question: "What does CSS stand for?",
+        question: "How do you define a function?",
         answers: [
-            {text: "Cascsding Style Sheet", correct: true},
-            {text: "Can Sometimes Style", correct: false},
-            {text: "Cascading Side Sheet", correct: false},
-            {text: "Create Some Styles", correct: false}
+            {text: "function() {}", correct: true},
+            {text: "function.log", correct: false},
+            {text: "function['x', 'y']", correct: false},
+            {text: "none of the above", correct: false}
         ]
     },
     {
@@ -48,50 +30,189 @@ var questions = [
         ]
     },
     {
-        question: "Lorem ipsum",
+        question: "If a variable is global..",
         answers: [
-            {text: "" , correct: false},
-            {text: "" , correct: false},
-            {text: "" , correct: false},
-            {text: "" , correct: true}
+            {text: "it can be used by anyone, anywhere." , correct: false},
+            {text: "it can never be used." , correct: false},
+            {text: "it can only be used inside a specified function." , correct: false},
+            {text: "it can be used on any function within the file." , correct: true}
         ]
     },
     {
-        question: "",
+        question: "'string' is an example of:",
         answers: [
-            {text: "" , correct: false},
-            {text: "" , correct: false},
-            {text: "" , correct: true},
-            {text: "" , correct: false}
+            {text: "An array." , correct: false},
+            {text: "A variable." , correct: false},
+            {text: "A data type." , correct: true},
+            {text: "A method." , correct: false}
         ]
     }
 ];
 
-//Start quiz
-function startQuiz(event) {
-    event.defaultPrevented;
-    console.log('started');
-    startBtn.classList.add('hide');
+var startBtn = document.getElementById("startBtn");
+var questionEl = document.getElementById("question");
+var answerBtn = document.getElementById("answer-choices");
+var nextBtn = document.getElementById("nextBtn");
+var timerP = document.getElementById('timer')
+var timer = 0;
+
+
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+var timeInterval
+
+
+// GIVEN I am taking a code quiz
+//WHEN I click the start button
+
+
+
+
+function startTimer(){
+    timer = 20
+    timeInterval = setInterval( function(){
+        timer--
+        timerP.textContent = timer + " seconds left" 
+        if (timer === 0) {
+            clearInterval(timeInterval)
+            alert("Time's up, better luck next time!")
+            showScore()
+        }
+    }, 1000) 
+}
+
+
+
+
+startBtn.addEventListener("click", function(event){
+    var welcome = document.getElementById("welcome")
+    var quizCont = document.getElementById("quiz")
+    welcome.style.display = "none";
+    quizCont.style.display ="block";
+   
+    startQuiz();
+   
+   
+})
+
+
+
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextBtn.innerHTML = "Next >"
+    showQuestion();  
+    startTimer(); 
+
+   
+}
+
+
+
+
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionEl.innerHTML = questionNo + ") " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerHTML =answer.text;   
+        button.classList.add("ansBtn");
+        answerBtn.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+    });
+
+
+    //THEN a timer starts and I am presented with a question
+    //WHEN I answer a question
+    //THEN I am presented with another question
+    //WHEN I answer a question incorrectly
+    //THEN time is subtracted from the clock    <----------------------------------------- TO DO
+
+
+
+
+
 
 }
 
-//Start timer
-function timer(){
+function resetState(){
+    nextBtn.style.display = "none";
+    while(answerBtn.firstChild){
+        answerBtn.removeChild(answerBtn.firstChild);
+
+    }
+}
+
+function selectAnswer(event){
+    var selectBtn = event.target;
+    var isCorrect = selectBtn.dataset.correct === "true";
+        if(isCorrect){
+            selectBtn.classList.add("correct");
+            score++;
+        }
+        else {
+            selectBtn.classList.add("wrong");
+           
+
+        }
+        Array.from(answerBtn.children).forEach(button => {
+            if (button.dataset.correct === "true"){
+                button.classList.add("correct");
+            }
+            button.disabled = true;
+        });
+        nextBtn.style.display = "block";
+    
+}
+
+
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and my score     <-------------------------------- TO DO
+
+
+function showScore(){
+    resetState();
+    clearInterval(timeInterval);
+    questionEl.innerHTML = "You scored: "+ score + " .";   
+    nextBtn.innerHTML = "Try Again";
+    nextBtn.style.display = "block"  
+    localStorage.setItem("score", score);
+
+
+  
+}
+
+function handleNextBtn(){
+currentQuestionIndex++;
+if(currentQuestionIndex < questions.length && timer > 0){
+    showQuestion()
+}else {
+    showScore();
+}
+}
+
+function handleTryAgain() {
 
 }
 
-// Questions
-function nxtQuestion() {
+nextBtn.addEventListener('click', () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextBtn();
+    } else 
+    startQuiz();
+})
 
-}
+//startQuiz();
 
-//Answers
-function selectAnswer() {
-
-}
-
-//Game Over
-
-function gameOver(){
-
-}
